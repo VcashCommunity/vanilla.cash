2d7005e1417a78d3cd7648cde9471d1ebd447fa0
wallisonalves <wallisonalvesdeoliveira@gmail.com>
2018-12-03 11:58:26 -0200

    vanilla

--- inc/message.php
+++ inc/message.php
@@ -1,5 +1,5 @@
   <div class="alert alert-dismissable alert-warning">
     <span class="glyphicon glyphicon-warning-sign">
     </span>
-    <button type="button" class="close" data-dismiss="alert">× </button> Version 0.6.0.4 binaries are outdated bootstrap nodes, update their nodes through the vanilla branch source code. Current users of 0.5.1 and versions will have to resynchronize the chain. Make backups before any action!!!
+    <button type="button" class="close" data-dismiss="alert">× </button> Version 0.6.0.4 binaries are outdated bootstrap nodes, update their nodes through the <a href="https://github.com/vcashcommunity/vcash/tree/vanilla" target="_blank">vanilla</a>, branch source code. Current users of 0.5.1 and versions will have to resynchronize the chain. Make backups before any action!!!
   </div>
